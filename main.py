43e973ed03766fa9fdc9636372a13c9b3134c81f
Stephen <armando2002@gmail.com>
2022-12-19 23:31:33 -0800

    Got spaceship loaded on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # sets height and width
 WIDTH, HEIGHT = 900, 500
@@ -6,10 +7,28 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 # sets window title
 pygame.display.set_caption("First Game")
 
+# define screen background
 WHITE = (255, 255, 255)
+
+# define FPS
+FPS = 60
+
+# import images for spaceships using OS
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+
+
+def draw_window():
+    # give white background
+    WIN.fill(WHITE)
+    # draw images
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    pygame.display.update()
 
 
 def main():
+    # controls speed of while loop at 60 FPS
+    clock = pygame.time.Clock()
     run = True
     while run:
         # check for events in pygame
@@ -17,10 +36,8 @@ def main():
             # if user quits
             if event.type == pygame.QUIT:
                 run = False
-
-        WIN.fill(WHITE)
-        # give white background
-        pygame.display.update()
+        # refresh the screen each loop
+        draw_window()
 
     pygame.quit()
 
