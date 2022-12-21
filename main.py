0c94e691e48fa267ee523097fa7a7d45a81028ea
Stephen <armando2002@gmail.com>
2022-12-20 19:14:42 -0800

    added 2nd spaceship

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 # resize image to be smaller & rotate
 YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,
-    (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+                                                                        (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP_IMAGE = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,
+                                                                     (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
 def draw_window():
@@ -28,6 +29,7 @@ def draw_window():
     WIN.fill(WHITE)
     # draw images
     WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    WIN.blit(RED_SPACESHIP_IMAGE, (700, 100))
     pygame.display.update()
 
 
